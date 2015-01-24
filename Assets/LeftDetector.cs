a751d8df096de8f6062bc1ab8d01c897dbdb6077
Ironnz <zjansz@yahoo.com>
2015-01-24 12:00:54 -0600

    Updating, hopefully this will work.

---
+++ Assets/LeftDetector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftDetector : MonoBehaviour {
+	bool IsTouching = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "wall") {
+			IsTouching = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "wall") {
+			IsTouching = false;
+		}
+	}
+	public bool CheckIt(){
+		return IsTouching;
+	}
+}
