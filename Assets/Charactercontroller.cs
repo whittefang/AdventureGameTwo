804a94551f2a3a663d798c2dd388142db832f89e
whittefang <whittefang@gmail.com>
2015-01-23 20:21:22 -0600

    character controller
    added character controller

---
+++ Assets/Charactercontroller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charactercontroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)){
+			//transform.position = new Vector2(transform.position.y, transform.transform.position.y +1);
+
+			transform.Translate(new Vector3(0,5,0) * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.S)){
+			//transform.position = new Vector2(transform.position.y, transform.transform.position.y +1);
+			
+			transform.Translate(new Vector3(0,-5,0) * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A)){
+			//transform.position = new Vector2(transform.position.y, transform.transform.position.y +1);
+			
+			transform.Translate(new Vector3(-5,0,0) * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			//transform.position = new Vector2(transform.position.y, transform.transform.position.y +1);
+			
+			transform.Translate(new Vector3(5,0,0) * Time.deltaTime);
+		}
+	
+	}
+}
